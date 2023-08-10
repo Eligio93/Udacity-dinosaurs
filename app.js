dfb11d7db9cd36e326a08177dacce6919a41ee57
Eligio93 <eligio.cristantielli@gmail.com>
2023-08-10 10:24:03 +1000

    create IIFE to cllect human data

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const form = document.getElementById("dino-compare");
-form.style.display = "none";
+
 // Create Dino Constructor
 function Dino(species, weight, height, diet, where, when, fact) {
 
     this.species = species,
-    this.weight = weight
+        this.weight = weight
     this.height = height
     this.diet = diet
     this.where = where
@@ -19,22 +18,16 @@ let dinos = [
     new Dino("Triceratops", 13000, 114, "herbavor", "North America", "Late Cretaceous", "First discovered in 1889 by Othniel Charles Marsh"),
     new Dino("Tyrannosaurus Rex", 11905, 144, "carnivor", "North America", "Late Cretaceous", "The largest known skull measures in at 5 feet long."),
     new Dino("Anklyosaurus", 10500, 55, "herbavor", "North America", "Late Cretaceous", "Anklyosaurus survived for approximately 135 million years."),
-    new Dino("Brachiosaurus",70000,372,"herbavor","North America","Late Jurasic","An asteroid was named 9954 Brachiosaurus in 1991."),
-    new Dino("Stegosaurus",11600,79,"herbavor","North America, Europe, Asia","Late Jurasic to Early Cretaceous","The Stegosaurus had between 17 and 22 seperate places and flat spines."),
-    new Dino("Elasmosaurus",16000,59,"carnivor","North America","Late Cretaceous","Elasmosaurus was a marine reptile first discovered in Kansas."),
-    new Dino("Pteranodon",44,20,"carnivor","North America","Late Cretaceous","Actually a flying reptile, the Pteranodon is not a dinosaur."),
-    new Dino("Pigeon",0.5,9,"herbavor","World Wide","Holocene","All birds are living dinosaurs.")
+    new Dino("Brachiosaurus", 70000, 372, "herbavor", "North America", "Late Jurasic", "An asteroid was named 9954 Brachiosaurus in 1991."),
+    new Dino("Stegosaurus", 11600, 79, "herbavor", "North America, Europe, Asia", "Late Jurasic to Early Cretaceous", "The Stegosaurus had between 17 and 22 seperate places and flat spines."),
+    new Dino("Elasmosaurus", 16000, 59, "carnivor", "North America", "Late Cretaceous", "Elasmosaurus was a marine reptile first discovered in Kansas."),
+    new Dino("Pteranodon", 44, 20, "carnivor", "North America", "Late Cretaceous", "Actually a flying reptile, the Pteranodon is not a dinosaur."),
+    new Dino("Pigeon", 0.5, 9, "herbavor", "World Wide", "Holocene", "All birds are living dinosaurs.")
 ];
-console.log(dinos)
+console.log(dinos);
 // Create Human Object
-const human = {
-    name: "Eligio",
-    height: 100,
-    diet: "carnivor",
-    weight: 100
-}
-// Use IIFE to get human data from form
 
+// Use IIFE to get human data from form
 
 
 
@@ -74,27 +67,38 @@ Dino.prototype.compareWeight = function () {
 
 
 // Generate Tiles for each Dino in Array
-dinos.forEach(function (object) {
-    //Generate Random fact about dinos
-    let compareArray = [object.compareDiet, object.compareHeight, object.compareWeight, object.fact]
-    let randomChoice = compareArray[Math.floor(Math.random() * compareArray.length)];
-    if(randomChoice !== object.fact && object.species !== "Pigeon"){
-       randomChoice.call(object);
-    }
-    let grid=document.getElementById("grid");
-    let tile=document.createElement("div");
-    tile.id=object.species;
-    tile.className="grid-item";
-    let bgimg=document.createElement("img");
-    let imgtitle= (object.species).toLowerCase();
-    const imageUrl = `images/${imgtitle}.png`;
-    bgimg.src=imageUrl;
-    tile.appendChild(bgimg);
-   
-    
 
-    grid.appendChild(tile);
-})
+let generateTiles = function () {
+    dinos.forEach(function (object) {
+        //Generate Random fact about dinos
+        let compareArray = [object.compareDiet, object.compareHeight, object.compareWeight, object.fact]
+        let randomChoice = compareArray[Math.floor(Math.random() * compareArray.length)];
+        if (randomChoice !== object.fact && object.species !== "Pigeon") {
+            randomChoice.call(object);
+        }
+        //Generate tiles
+        let grid = document.getElementById("grid");
+        let tile = document.createElement("div");
+        tile.id = object.species;
+        tile.className = "grid-item";
+        //Set tiles name
+        let name = document.createElement("h3");
+        name.textContent = object.species;
+        tile.appendChild(name);
+        //Set Image to tiles
+        let bgimg = document.createElement("img");
+        let imgtitle = (object.species).toLowerCase();
+        const imageUrl = `images/${imgtitle}.png`;
+        bgimg.src = imageUrl;
+        tile.appendChild(bgimg);
+
+        //Set tiles fact
+        let dinofact = document.createElement("p");
+        dinofact.textContent = object.fact;
+        tile.appendChild(dinofact);
+        grid.appendChild(tile);
+    })
+}
 
 
 
@@ -104,3 +108,22 @@ dinos.forEach(function (object) {
 
 
 // On button click, prepare and display infographic
+let compareBtn=document.getElementById("btn");
+compareBtn.addEventListener("click", function(){
+    let human = (function(){
+        let name=document.getElementById("name").value;
+        let feet= document.getElementById("feet").value;
+        let inches= document.getElementById("inches").value;
+        let height= parseFloat(feet.toString()+ "." + inches.toString());
+        let weight= document.getElementById("weight").value;
+        let diet= document.getElementById("diet").value;
+
+        return{
+            name,
+            height,
+            weight,
+            diet
+        }
+    })();
+    
+})
